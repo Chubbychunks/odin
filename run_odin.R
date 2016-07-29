c47308cd2e80fe86a58ef16b82a2f82693ca46e6
Eugene Geidelberg <eg1012@dide.local>
2016-07-29 15:47:17 +0100

    easier way to change one parameter and keep the rest the same. for model checking in the future.

--- run_odin.R
+++ run_odin.R
@@ -7,12 +7,142 @@ require(reshape2)
 
 
 # gen <- odin::odin("ODEs_odin.R")
-gen <- odin::odin("ODEs_odin_with_2_groups.R")
+gen <- odin::odin("ODEs_odin_with_2_groups.R", ".")
 
 
 time <- seq(0, 100, length.out = 100)
 
 groups <- c("FSW", "Clients")
+
+generate_parameters <- function(...) {
+  defaults <- list(S0_init = c(2000,2000),
+                   S1a_init = c(0,0),
+                   S1b_init = c(0,0),
+                   S1c_init = c(0,0),
+                   I01_init = c(0,1000),
+                   I11_init = c(0,0),
+                   I02_init = c(0,0),
+                   I03_init = c(0,0),
+                   I04_init = c(0,0),
+                   I05_init = c(0,0),
+                   I22_init = c(0,0),
+                   I23_init = c(0,0),
+                   I24_init = c(0,0),
+                   I25_init = c(0,0),
+                   I32_init = c(0,0),
+                   I33_init = c(0,0),
+                   I34_init = c(0,0),
+                   I35_init = c(0,0),
+                   I42_init = c(0,0),
+                   I43_init = c(0,0),
+                   I44_init = c(0,0),
+                   I45_init = c(0,0),
+                   
+                   
+                   mu = c(0.02,0.02),
+                   gamma01 = c(0.2,0.2),
+                   gamma02 = c(0.2,0.2),
+                   gamma03 = c(0.2,0.2),
+                   gamma04 = c(0.2,0.2),
+                   
+                   gamma11 = c(0.2,0.2),
+                   
+                   gamma22 = c(0.2,0.2),
+                   gamma23 = c(0.2,0.2),
+                   gamma24 = c(0.2,0.2),
+                   
+                   gamma32 = c(0.2,0.2),
+                   gamma33 = c(0.2,0.2),
+                   gamma34 = c(0.2,0.2),
+                   
+                   gamma42 = c(0.2,0.2),
+                   gamma43 = c(0.2,0.2),
+                   gamma44 = c(0.2,0.2),
+                   
+                   rho2 = c(0.1,0.1),
+                   rho3 = c(0.1,0.1),
+                   rho4 = c(0.1,0.1),
+                   rho5 = c(0.1,0.1),
+                   
+                   phi2 = c(0.04,0.01),
+                   phi3 = c(0.04,0.01),
+                   phi4 = c(0.04,0.01),
+                   phi5 = c(0.04,0.01),
+                   
+                   psia = c(1,1),
+                   psib = c(1,1),
+                   
+                   tau01 = c(0.5,0.5),
+                   tau11 = c(0.5,0.5),
+                   tau2 = c(0.5,0.5),
+                   tau3 = c(0.5,0.5),
+                   tau4 = c(0.5,0.5),
+                   tau5 = c(0.5,0.5),
+                   
+                   zetaa = c(0.1,0.1),
+                   zetab = c(0.1,0.1),
+                   zetac = c(0.1,0.1),
+                   
+                   alpha01 = c(0.01,0.01),
+                   alpha02 = c(0.01,0.01),
+                   alpha03 = c(0.01,0.01),
+                   alpha04 = c(0.01,0.01),
+                   alpha05 = c(1,1),
+                   
+                   alpha11 = c(0.01,0.01),
+                   
+                   alpha21 = c(0.01,0.01),
+                   alpha22 = c(0.01,0.01),
+                   alpha23 = c(0.01,0.01),
+                   alpha24 = c(0.01,0.01),
+                   alpha25 = c(1,1),
+                   
+                   alpha32 = c(0.01,0.01),
+                   alpha33 = c(0.01,0.01),
+                   alpha34 = c(0.01,0.01),
+                   alpha35 = c(1,1),
+                   
+                   alpha42 = c(0.01,0.01),
+                   alpha43 = c(0.01,0.01),
+                   alpha44 = c(0.01,0.01),
+                   alpha45 = c(1,1),
+                   
+                   
+                   beta = c(0.193,0.182),
+                   #beta = 0,
+                   c = c(4,6),
+                   ec = c(0.85,0.84),
+                   # ec = c(1,1),
+                   
+                   eP = c(0.6,0.5),
+                   epsilon = 0.001,
+                   # fc = c(1,1),
+                   fc = c(0.9,0.9),
+                   
+                   fP = c(0.5,0.3),
+                   n = c(10,3),
+                   #n = 0,
+                   
+                   R = c(1,1),
+                   omega = c(0.5, 0.5),
+                   theta = 0.5 
+  )
+  given <- list(...)
+  if (length(given) == 0L) {
+    return(defaults)
+  }
+  if (is.null(names(given)) || !all(nzchar(names(given)))) {
+    stop("All arguments must be named")
+  }
+  extra <- setdiff(names(given), names(defaults))
+  if (length(extra) > 0L) {
+    stop("Unknown arguments: ", extra)
+  }
+  modifyList(defaults, given)
+}
+
+generate_parameters()
+generate_parameters(theta=1)
 
 parameters <- list(S0_init = c(2000,2000),
                    S1a_init = c(0,0),
@@ -208,8 +338,6 @@ naming_function <- function(xx){
 
 
 
-
-
 vary2 <- function(x, base, v, gen, time) {
   n <- lengths(base[v])
   base[v] <- setNames(split(unlist(x, use.names=FALSE), rep(seq_along(v), n)), v) # base is the original set of pars
@@ -230,6 +358,8 @@ vary2 <- function(x, base, v, gen, time) {
 }
 
 output <- lapply(seq_len(nrow(pp)), function(i) vary2(pp[i,], parameters, v, gen, time))
+
+
 
 head(output[[1]]$S0)
 head(output[[1]]$I01)
@@ -270,6 +400,38 @@ for(p in 1:length(pp[,1]))
   print(plot)
 }
 #lapply(output, head)
+
+
+
+f <- function(x, base, v, gen, time) {
+  n <- lengths(base[v])
+  base[v] <- setNames(split(unlist(x, use.names=FALSE), rep(seq_along(v), n)), v) # base is the original set of pars
+  mod <- gen(user=base)
+  mod$transform_variables(mod$run(time))
+}
+
+
+tmp <- lapply(seq_len(nrow(pp)), function(i) f(pp[i,], parameters, v, gen, time))
+names(tmp[[1]])
+
+x <- tmp[[1]]
+x[grep("^[SI]", names(tmp[[1]]))]
+xx <- x[grep("^[SI]", names(tmp[[1]]))]
+
+rowSums(do.call(cbind, xx))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # DISCARDED NONSENSE
